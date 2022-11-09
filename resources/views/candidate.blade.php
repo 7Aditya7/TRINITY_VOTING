ee6cc644a6627b3f1e29e87a3135177f8e698d74
Laure-Sagar <slaure354@gmail.com>
2022-11-10 00:15:23 +0545

    Feat: La Done My Part.

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -2,100 +2,99 @@
 
 @section('content')
 <section>
-  <div class="container mt-1 mb-5 p-2">
-    <div class="my-4">
-      <div class="candidate">
-        <h3>Candidates Search</h3>
-        <hr>
-      </div>
-      <div class="row g-0">
-        <div class="col-xl-8">
-          <div class="card-body ml-5 text-black">
-            <form action="/candidate" method="POST">
-              @csrf
-              <div class="mb-4">
-                <div class="row">
-                  <div class="col-4 form-group">
-                    <select class="form-control Province" id="exampleFormControlSelect2" onchange="showDistrict(this)"
-                      name="province">
-                      <option selected="true" disabled selected>-- Faculty --</option>
-                      <option value="csit">CSIT</option>
-                      <option value="bca">BCA</option>
-                      <option value="bba">BBA</option>
-                      <option value="Gandaki Province">Gandaki Province</option>
-                      <option value="Lumbini Province">Lumbini Province</option>
-                      <option value="Karnali Province">Karnali Province</option>
-                      <option value="Sudurpashchim Province">Sudurpashchim Province</option>
-                    </select>
-                  </div>
+  <div class="container mt-1 p-2">
+    <div class="candidate">
+      <h3>Trinity Election 2022 | Candidates Search</h3>
+      <hr>
+    </div>
+    <div class="row g-0">
+      <div class="col-xl-8">
+        <div class="card-body ml-5 text-black">
+          <form action="/candidate" method="POST">
+            @csrf
+            <div class="mb-4">
+              <div class="row">
+                <div class="col-4 form-group">
+                  <select class="form-control Province" id="exampleFormControlSelect2" onchange="showDistrict(this)"
+                    name="province">
+                    <option selected="true" disabled selected>-- Faculty --</option>
+                    <option value="csit">CSIT</option>
+                    <option value="bca">BCA</option>
+                    <option value="bba">BBA</option>
+                    <option value="Gandaki Province">Gandaki Province</option>
+                    <option value="Lumbini Province">Lumbini Province</option>
+                    <option value="Karnali Province">Karnali Province</option>
+                    <option value="Sudurpashchim Province">Sudurpashchim Province</option>
+                  </select>
+                </div>
 
-                  <div class="col-4 form-group" id="noDist" style="display:none;">
-                    <select class="form-control District" name="district">
-                      <option selected="true" disabled="disabled">-- Batch --</option>
-                      <option value="Dolakha">Dolakha</option>
-                      <option value="Sindhupalchok">Sindhupalchok</option>
-                      <option value="Rasuwa">Rasuwa</option>
-                      <option value="Dhading">Dhading</option>
-                      <option value="Nuwakot">Nuwakot</option>
-                      <option value="Kathmandu">Kathmandu</option>
-                      <option value="Bhaktapur">Bhaktapur</option>
-                      <option value="Lalitpur">Lalitpur</option>
-                      <option value="Kavrepalanchok">Kavrepalanchok</option>
-                      <option value="Ramechhap">Ramechhap</option>
-                      <option value="Sindhuli">Sindhuli</option>
-                      <option value="Makwanpur">Makwanpur</option>
-                      <option value="Chitwan">Chitwan</option>
-                    </select>
-                  </div>
-                  <div class="col-4 form-group">
-                    <button type="submit" class="btn btn-danger" name="search" value="search">Search</button>
-                  </div>
+                <div class="col-4 form-group" id="noDist" style="display:none;">
+                  <select class="form-control District" name="district">
+                    <option selected="true" disabled="disabled">-- Batch --</option>
+                    <option value="Dolakha">Dolakha</option>
+                    <option value="Sindhupalchok">Sindhupalchok</option>
+                    <option value="Rasuwa">Rasuwa</option>
+                    <option value="Dhading">Dhading</option>
+                    <option value="Nuwakot">Nuwakot</option>
+                    <option value="Kathmandu">Kathmandu</option>
+                    <option value="Bhaktapur">Bhaktapur</option>
+                    <option value="Lalitpur">Lalitpur</option>
+                    <option value="Kavrepalanchok">Kavrepalanchok</option>
+                    <option value="Ramechhap">Ramechhap</option>
+                    <option value="Sindhuli">Sindhuli</option>
+                    <option value="Makwanpur">Makwanpur</option>
+                    <option value="Chitwan">Chitwan</option>
+                  </select>
+                </div>
+                <div class="col-4 form-group">
+                  <button type="submit" class="btn btn-danger" name="search" value="search">Search</button>
                 </div>
               </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    </form>
+
+    {{-- SHOW CANDIDATE --}}
+    <div class="row m-4">
+      <div class="card mx-4 shadow-sm" style="width: 15rem;">
+        <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
+        <div class="card-body row">
+          <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
+          <div class="">
+            <h5 class="text-center card-title">Bottle</h5>
+            <h6 class="text-center">Sagar Chhetri</h6>
+            <h6 class="text-center">CSIT( 2018 )</h6>
+
           </div>
         </div>
       </div>
-      </form>
+      <div class="card mx-4 shadow-sm" style="width: 15rem;">
+        <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
+        <div class="card-body row">
+          <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
+          <div class="">
+            <h5 class="text-center card-title">Bottle</h5>
+            <h6 class="text-center">Sagar Chhetri</h6>
+            <h6 class="text-center">CSIT( 2018 )</h6>
 
-      {{-- SHOW CANDIDATE --}}
-      <div class="row m-4">
-        <div class="card mx-4 shadow-sm" style="width: 15rem;">
-          <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
-          <div class="card-body row">
-            <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
-            <div class="">
-              <h5 class="text-center card-title">Bottle</h5>
-              <h6 class="text-center">Sagar Chhetri</h6>
-              <h6 class="text-center">CSIT( 2018 )</h6>
-
-            </div>
           </div>
         </div>
-        <div class="card mx-4 shadow-sm" style="width: 15rem;">
-          <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
-          <div class="card-body row">
-            <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
-            <div class="">
-              <h5 class="text-center card-title">Bottle</h5>
-              <h6 class="text-center">Sagar Chhetri</h6>
-              <h6 class="text-center">CSIT( 2018 )</h6>
-
-            </div>
-          </div>
-        </div>
-        <div class="card mx-4 shadow-sm" style="width: 15rem;">
-          <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
-          <div class="card-body row">
-            <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
-            <div class="">
-              <h5 class="text-center card-title">Bottle</h5>
-              <h6 class="text-center">Sagar Chhetri</h6>
-              <h6 class="text-center">CSIT( 2018 )</h6>
-
-            </div>
-          </div>
-        </div>
-
       </div>
+      <div class="card mx-4 shadow-sm" style="width: 15rem;">
+        <img src="{{asset('image/candidate.png')}}" class="card-img-top" alt="...">
+        <div class="card-body row">
+          <img src="{{asset('image/bottle.png')}}" style="width: 5rem;" alt="...">
+          <div class="">
+            <h5 class="text-center card-title">Bottle</h5>
+            <h6 class="text-center">Sagar Chhetri</h6>
+            <h6 class="text-center">CSIT( 2018 )</h6>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
 </section>
 @endsection
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,115 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-<section>
-  <div class="candidate">
-    <h3>RESULT</h3>
+<section class="container">
+  <div class="candidate mt-1 p-2">
+    <h3>Trinity Election 2022 | Result</h3>
+    <hr>
   </div>
-  <div class="container py-5 h-100 mt-1 mb-5">
+  <div class="container h-100 mt-1">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col">
-        <div class="card  my-4">
-          <div class="row g-0">
-            <div class="col-xl-4 d-none d-xl-block">
-              <img src="./image/Logo.png" alt="Logo" class="img-fluid d-flex justify-content-center bodyLogo" />
-            </div>
-            <div class="col-xl-8">
-              <div class="card-body p-md-5 text-black">
-                <form action="/result" method="POST">
-                  @csrf
-                  <label class="form-label" for="form3Example8">Address</label>
-                  <div class="form-outline mb-4">
-
-                    <div class="row">
-                      <div class="col-md-6 mb-4 col-xl-4">
-                        <select class="Province" onchange="showDistrict(this)" name="province">
-                          <!-- used function showDistrict() on section of Province
-                      to display corresponding districts -->
-
-                          <!--set label as disabled/non-selectable option-->
-                          <option selected="true" disabled="disabled">-- Province --</option>
-
-                          <!-- All of province are valued as their names-->
-                          <option value="Province 1">Province 1</option>
-                          <option value="Madhesh Province">Madhesh Province</option>
-                          <option value="Bagmati Province">Bagmati Province</option>
-                          <option value="Gandaki Province">Gandaki Province</option>
-                          <option value="Lumbini Province">Lumbini Province</option>
-                          <option value="Karnali Province">Karnali Province</option>
-                          <option value="Sudurpashchim Province">Sudurpashchim Province</option>
-                        </select>
-                      </div>
-
-                      <div class="col-md-6 mb-4 col-xl-2" id="noDist" style="display:none;">
-                        <select class="District">
-                          <option selected="true" disabled="disabled">-- District --</option>
-                          <option value="0">Not Available</option>
-                        </select>
-                      </div>
-
-                      <div class="col-md-6 mb-4 col-xl-2" id="Prv3Dist" style="display:none;">
-                        <select class="District" name="district">
-                          <!--set label as disabled/non-selectable option-->
-                          <option selected="true" disabled="disabled">-- District --</option>
-                          <!-- All of options below are valued as their names-->
-                          <option value="Dolakha">Dolakha</option>
-                          <option value="Sindhupalchok">Sindhupalchok</option>
-                          <option value="Rasuwa">Rasuwa</option>
-                          <option value="Dhading">Dhading</option>
-                          <option value="Nuwakot">Nuwakot</option>
-                          <option value="Kathmandu">Kathmandu</option>
-                          <option value="Bhaktapur">Bhaktapur</option>
-                          <option value="Lalitpur">Lalitpur</option>
-                          <option value="Kavrepalanchok">Kavrepalanchok</option>
-                          <option value="Ramechhap">Ramechhap</option>
-                          <option value="Sindhuli">Sindhuli</option>
-                          <option value="Makwanpur">Makwanpur</option>
-                          <option value="Chitwan">Chitwan</option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary btn-sm" name="search" value="search">Search</button>
-
-                  </div>
-
-
-              </div>
-            </div>
-            </form>
-            @php
-            $result= $res->result;
-            @endphp
-
-
-            @if ($result==1)
-            @if($can)
-
-            <div class="winnerContainer border rounded">
-              <h5>{{ $can->province }} : {{ $can->district }}</h5>
+        <div class="row g-0">
+          <div class="col-xl-4 d-none d-xl-block">
+            <img src="./image/logo.png" alt="Logo" class="img-fluid d-flex justify-content-center bodyLogo" />
+          </div>
+          </form>
+          <div class="winnerContainer shadow-sm  text-center border rounded">
+            <div class="p-4">
+              <h5>CSIT: 2018 BATCH</h5>
               <h3>!!! WINNER !!!</h3>
-
               <div class="winner">
-                <img src="{{ url('candidateImage/'.$can->photo) }}"
-                  style="width: 70px; height: 70px; border-radius:50%; padding:0.1rem">
-
+                <img src="{{ asset('image/candidate.png') }}" class="rounded-circle" style="width: 10rem">
               </div>
-              <h3>{{$can->name}} ({{ $can->partyName }}) <img src="{{ url('candidateImage/'.$can->partyLogo) }}"
+              <h3>Sagar Chhetri - Bottle <img src="{{ asset('image/bottle.png') }}"
                   style="width: 40px; height: 40px; border-radius:50%; padding:0.1rem">
               </h3>
-              <h3><b>Votes:</b> {{$can->votes}} </h3>
+              <h3><b>Votes:</b> 150 </h3>
             </div>
-
-            @endif
-            @else
-            <div class="msg d-flex justify-content-center mb-3">
-              <h4>Result is not published.</h4>
-
-              @endif
-
-
-            </div>
-
           </div>
+          <div class="msg d-flex justify-content-center mb-3">
+            <h4>Result is not published.</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </section>
 @endsection
